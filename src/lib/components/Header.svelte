42b85b59c8b70846e09769e6ea341b9aabe41e9d
Russell Warner <rewarner@russellbits.com>
2022-11-03 16:20:12 +1100

    Initial commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,2 @@
+<h1>Banapana</h1>
+<h2>Your mind on media</h2>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+import Header from '$lib/components/Header.svelte';
+import '$lib/stylesheets/house.scss';
+</script>
+
+<Header/>
+<p>This is the new and future re-build of Banapana.</p>
+<p>See <a href="https://medium.com/minds-on-media">Banapana on Medium</a> for current publications.</p>
